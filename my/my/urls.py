65801efedd7e1a6f271391f6dc4c0e87ec4b90a8
Roman3122 <53575561+Roman3122@users.noreply.github.com>
2020-07-10 16:15:51 +0545

    Delete urls.py

--- my/my/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from django.urls import include
-urlpatterns = [
-	path('',include('web.urls')),
-    path('admin/', admin.site.urls),
-]
